9088ae746622325ca8bf47309b41ac9c2b271641
BrayLaak <47552683+BrayLaak@users.noreply.github.com>
2022-05-26 13:59:41 -0400

    Knowledge check first commit

---
+++ KC_1.py
@@ -0,0 +1,25 @@
+# Knowledge check 1
+
+# A statement that prints "hello world!"
+
+print("hello world!")
+
+
+# A list populated with several values. They can be strings / integers / floats etc.
+# Then, print() one of those values either to the prompt
+# or in a cell (if using Jupyter Notebook)
+
+aList = ["A string", 4, 3.2]
+print(aList[1])
+
+# A dictionary populated with two keys and two values. 
+# Print one of the values just like above. 
+
+aDictionary = {"key1": "value1", "key2": "value2"}
+print(aDictionary["key2"])
+
+
+# a tuple with 4 values. Print one of them.
+
+aTuple = (5, 6, 3, 123)
+print(aTuple[-2])
